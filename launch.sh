9a825fd608b393761a8f63a932469b3145658630
X3N0-Life-Form <adrarsis@laposte.net>
2015-02-26 09:22:40 +0100

    launch: -k + sleep for 1 sec between runs

--- launch.sh
+++ launch.sh
@@ -5,7 +5,8 @@ select="tournament"
 # cross_point, cross_uniform
 cross="cross_point"
 # bit_flip, k_flip
-mutate="bit_flip"
+mutate="k_flip"
+k=3
 # compare_with_parents, age, replace_worst
 insert="replace_worst"
 
@@ -22,5 +23,6 @@ fi
 for i in `seq 1 $loopCount`
 do
     echo "Loop #$i/$loopCount"
-    ./One-Future -select $select -cross $cross -mutate $mutate -insert $insert
+    ./One-Future -select $select -cross $cross -mutate $mutate -k $k -insert $insert
+    sleep 1
 done

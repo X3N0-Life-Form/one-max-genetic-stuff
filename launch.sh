7864ab9a6a7343917b74ba60833da7a75e40d127
X3N0-Life-Form <adrarsis@laposte.net>
2015-02-26 08:25:00 +0100

    launch script

---
+++ launch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# best, random, worst_best, tournament
+select="tournament"
+# cross_point, cross_uniform
+cross="cross_point"
+# bit_flip, k_flip
+mutate="bit_flip"
+# compare_with_parents, age, replace_worst
+insert="replace_worst"
+
+loopCount=1
+
+make
+
+if [ "$#" -ge 1 ]
+then
+    echo "Setting loop count to $1"
+    loopCount=$1
+fi
+
+for i in `seq 1 $loopCount`
+do
+    echo "Loop #$i/$loopCount"
+    ./One-Future -select $select -cross $cross -mutate $mutate -insert $insert
+done
